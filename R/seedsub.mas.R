b2d2890aae0fdab293aa147c78796f2e8c483729
Noah Peart <noah.v.peart@gmail.com>
2016-02-01 21:52:02 -0500

    ups

---
+++ R/seedsub.mas.R
@@ -0,0 +1,6 @@
+##' Some exploration of the seedling preferences for various substrates on
+##' Mt. Moosilauke.  This package will just contain data files to be used
+##' in interactive applications.
+##' @name seedsub.mas
+##' @import data.table
+NULL
--- R/substrate.R
+++
@@ -1,7 +0,0 @@
-##' Some exploration of the seedling preferences for various substrates 
-##' on Mt. Moosilauke.  This package will just contain data files to be used
-##' in interactive applications.
-##'
-##' @name substrate
-##' @import data.table
-NULL
